75904d61e75fd6f425624a9e0dfb5706744129fa
Benji Cohen <suddensquirrel94@gmail.com>
2023-12-25 10:36:17 +0000

    Classes done

--- website/pages/about/our-classes/index.js
+++ website/pages/about/our-classes/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Layout from '../../layouts'
+import styles from "./index.module.css";
 import Footer from "../../modules/Footer";
+import ContentTitle from '../../modules/ContentTitle';
 
 export default function AboutOurClasses() {
     return (
@@ -9,6 +11,44 @@ export default function AboutOurClasses() {
                 <title>Sarah Byrne Acting Academy | About | Our Classes</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <ContentTitle
+                title = "Classes"
+                accentTitle = "Our"
+            />
+            <section className='content-wrapper' style={{
+                marginTop: `-2em`
+            }}>
+                <div className='content column'>
+                    <img src="/img/classes/Classes-timetable.webp" />
+                    <img src="/img/gallery/7.jpg" />
+                </div>
+            </section>
+            <ContentTitle
+                title = "FREE Trial"
+                accentTitle = "Book Your"
+            />
+            <section className='content-wrapper'>
+                <div className='content column'>
+                    <div className={styles.classRow}>
+                        <div class={`${styles.classDetails} flex column`}>
+                            <h3>Manchester</h3>
+                            <p className={styles.accent}>Secure your FREE trial place at our Manchester Class</p>
+                        </div>
+                        <div className="flex a-center j-center">
+                            <a href="mailto:academy@sarahbyrneactingacademy.co.uk?subject=SBAA Manchster Class." className='btn primary'>Book Now</a>
+                        </div>
+                    </div>
+                    <div className={styles.classRow}>
+                        <div class={`${styles.classDetails} flex column`}>
+                            <h3>Oldham</h3>
+                            <p className={styles.accent}>Secure your FREE trial place at our Oldham Class</p>
+                        </div>
+                        <div className="flex a-center j-center">
+                            <a href="mailto:academy@sarahbyrneactingacademy.co.uk?subject=SBAA Oldham Class." className='btn primary'>Book Now</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <Footer />
         </Layout>
     );
