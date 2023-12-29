13287c936f3cde02de54553e73db5d3ca04e9126
Benji Cohen <suddensquirrel94@gmail.com>
2023-12-29 12:25:55 +0000

    things

--- website/pages/modules/HomepageForm/index.js
+++ website/pages/modules/HomepageForm/index.js
@@ -1,15 +1,36 @@
+import Link from "next/link";
 import styles from "./index.module.css"
 
 export function Form() {
     return (
-        <form action="" className={`${styles.form} grid col-2`}>
-            <input type="text" placeholder="First Name" name="First Name" />
-            <input type="text" placeholder="Last Name" name="Last Name" />
-            <input type="email" placeholder="Email" name="Email" />
-            <input type="text" placeholder="Phone" name="Phone" />
-            <textarea type="textarea" placeholder="Type your message here..." name="Additional Notes"></textarea>
-            <input type="submit" value="Submit" />
-        </form>
+        <ul className={styles.contactForm}>
+            <li>Phone:&nbsp;<a href="tel:07803230999">07803 230999</a></li>
+            <li>Social Media:&nbsp;
+                <Link href="https://www.facebook.com/sarahbyrneactingacademy" target="_blank">
+                    <img src="/img/fb.webp" alt="f" style={{
+                        height: 32,
+                        width: 32,
+                        marginBottom: "-5px",
+                    }}/>
+                </Link>
+                <Link href="hhttps://www.instagram.com/sarahbyrneacting/" target="_blank">
+                    <img src="/img/insta.webp" alt="i" style={{
+                        height: 32,
+                        width: 32,
+                        marginBottom: "-5px",
+                    }}/>
+                </Link>
+            </li>
+            <li>Email:&nbsp;<a href="mailto:academy@sarahbyrneactingacademy.co.uk">academy@sarahbyrneactingacademy.co.uk</a></li>
+        </ul>
+        // <form action="" className={`${styles.form} grid col-2`}>
+        //     <input type="text" placeholder="First Name" name="First Name" />
+        //     <input type="text" placeholder="Last Name" name="Last Name" />
+        //     <input type="email" placeholder="Email" name="Email" />
+        //     <input type="text" placeholder="Phone" name="Phone" />
+        //     <textarea type="textarea" placeholder="Type your message here..." name="Additional Notes"></textarea>
+        //     <input type="submit" value="Submit" />
+        // </form>
     )
 }
 
